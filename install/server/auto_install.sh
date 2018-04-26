c63f01698af5d03e91e973778288806b34b93295
kevindwei <1475975528@qq.com>
2018-04-26 11:20:06 +0800

    其他

--- install/server/auto_install.sh
+++ install/server/auto_install.sh
@@ -32,15 +32,15 @@ else
 fi
 
 # 安装依赖
-#echo "####install depandencies####"
-#yum install -y epel-release
-#yum install -y gcc expect python-pip python-devel ansible smartmontools dmidecode libselinux-python git rsync dos2unix mysql-devel
+echo "####install depandencies####"
+yum install -y epel-release
+yum install -y gcc expect python-pip python-devel ansible smartmontools dmidecode libselinux-python git rsync dos2unix mysql-devel
 
 # build webssh
-#echo "build webssh"
-#/usr/bin/yum install -y nodejs
-#cd $cur_dir/vendor/WebSSH2
-#/usr/bin/npm install -g cnpm --registry=https://registry.npm.taobao.org
+echo "build webssh"
+/usr/bin/yum install -y nodejs
+cd $cur_dir/vendor/WebSSH2
+/usr/bin/npm install -g cnpm --registry=https://registry.npm.taobao.org
 #/usr/bin/cnpm install --production
 #/usr/bin/cnpm install forever -g
 #/usr/bin/npm config set registry http://registry.cnpmjs.org
@@ -48,70 +48,70 @@ fi
 #/usr/bin/npm install forever -g
 
 # 分发代码
-#if [ ! $cur_dir ] || [ ! $adminset_dir ]
-#then
-#    echo "install directory info error, please check your system environment program exit"
-#    exit 1
-#else
-#    rsync --delete --progress -ra --exclude '.git' $cur_dir/ $adminset_dir
-#fi
-#scp $adminset_dir/install/server/ansible/ansible.cfg /etc/ansible/ansible.cfg
+if [ ! $cur_dir ] || [ ! $adminset_dir ]
+then
+    echo "install directory info error, please check your system environment program exit"
+    exit 1
+else
+    rsync --delete --progress -ra --exclude '.git' $cur_dir/ $adminset_dir
+fi
+scp $adminset_dir/install/server/ansible/ansible.cfg /etc/ansible/ansible.cfg
 
 # install webssh
-#scp /var/opt/adminset/main/install/server/webssh/webssh.service /usr/lib/systemd/system/webssh.service
-#systemctl enable webssh.service
+scp /var/opt/adminset/main/install/server/webssh/webssh.service /usr/lib/systemd/system/webssh.service
+systemctl enable webssh.service
 
 #安装数据库
-#echo "####install database####"
-#echo "installing a new mariadb...."
-#yum install -y mariadb-server mariadb-devel
-#service mariadb start
-#chkconfig mariadb on
-#mysql -e "CREATE DATABASE if not exists adminset DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+echo "####install database####"
+echo "installing a new mariadb...."
+yum install -y mariadb-server mariadb-devel
+service mariadb start
+chkconfig mariadb on
+mysql -e "CREATE DATABASE if not exists adminset DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
 # 安装mongodb
-#echo "####install mongodb####"
-#echo "installing a new Mongodb...."
-#yum install -y mongodb mongodb-server
-#/bin/systemctl start mongod
-#/bin/systemctl enable mongod
+echo "####install mongodb####"
+echo "installing a new Mongodb...."
+yum install -y mongodb mongodb-server
+/bin/systemctl start mongod
+/bin/systemctl enable mongod
 
-# 安装主程序
-#echo "####install adminset####"
-#mkdir -p  ~/.pip
-#cat <<EOF > ~/.pip/pip.conf
-#[global]
-#index-url = http://mirrors.aliyun.com/pypi/simple/
+# 安装主程序#
+echo "####install adminset####"
+mkdir -p  ~/.pip
+cat <<EOF > ~/.pip/pip.conf
+[global]
+index-url = http://mirrors.aliyun.com/pypi/simple/
 
-#[install]
-#trusted-host=mirrors.aliyun.com
-#EOF
-#pip install --upgrade pip
-#pip install kombu==4.1.0
-#pip install celery==4.0.2
-#pip install billiard==3.5.0.3
-#pip install pytz==2017.2
-#pip install kombu==4.1.0
-#cd $adminset_dir/vendor/django-celery-results-master
-#python setup.py build
-#python setup.py install
+[install]
+trusted-host=mirrors.aliyun.com
+EOF
+pip install --upgrade pip
+pip install kombu==4.1.0
+pip install celery==4.0.2
+pip install billiard==3.5.0.3
+pip install pytz==2017.2
+pip install kombu==4.1.0
+cd $adminset_dir/vendor/django-celery-results-master
+python setup.py build
+python setup.py install
 
-#cd $adminset_dir
-#pip install -r requirements.txt
-#python manage.py makemigrations
-#python manage.py migrate
-#echo "please create your adminset' super admin:"
-#python manage.py createsuperuser
-#source /etc/profile
-#/usr/bin/mysql -e "insert into adminset.accounts_userinfo (password,username,email,is_active,is_superuser) values ('pbkdf2_sha256\$24000\$2odRjOCV1G1V\$SGJCqWf0Eqej6bjjxusAojWtZkz99vEJlDbQHUlavT4=','admin','admin@126.com',1,1);"
-#scp $adminset_dir/install/server/adminset.service /usr/lib/systemd/system
-#systemctl daemon-reload
-#chkconfig adminset on
-#service adminset start
+cd $adminset_dir
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+echo "please create your adminset' super admin:"
+python manage.py createsuperuser
+source /etc/profile
+/usr/bin/mysql -e "insert into adminset.accounts_userinfo (password,username,email,is_active,is_superuser) values ('pbkdf2_sha256\$24000\$2odRjOCV1G1V\$SGJCqWf0Eqej6bjjxusAojWtZkz99vEJlDbQHUlavT4=','admin','admin@126.com',1,1);"
+scp $adminset_dir/install/server/adminset.service /usr/lib/systemd/system
+systemctl daemon-reload
+chkconfig adminset on
+service adminset start
 
 #安装redis
-#echo "####install redis####"
-#yum install redis -y
+echo "####install redis####"
+yum install redis -y
 chkconfig redis on
 service redis start
 
@@ -174,7 +174,7 @@ service celery restart
 service beat restart
 service mongod restart
 service sshd restart
-#service webssh restart
+service webssh restart
 guacd service start guacd
 echo "please access website http://server_ip"
 echo "you have installed adminset successfully!!!"
